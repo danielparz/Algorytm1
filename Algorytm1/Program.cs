ff49fb3a7945037b0c0e5d6fb9ff328eac0f777a
Daniel <dparzynski@gmail.com>
2022-06-23 15:22:43 +0200

    Add project files.

---
+++ Algorytm1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace Algorytm1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseString = args[0].ToString();
+            List<string> substrings = new List<string>();
+
+            for(int i = 0; i < baseString.Length; i++)
+            {
+                for(int j = 0; j < baseString.Length - i; j++)
+                {
+                    substrings.Add(baseString.Substring(i, j + 1));
+                }
+            }
+
+            foreach(var substring in substrings)
+                Console.WriteLine(substring);
+        }
+    }
+}
